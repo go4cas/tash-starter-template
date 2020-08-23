08a7c5f34e6648367571b22aaf2b141ee0007611
Cas du Plessis <go4cas@gmail.com>
2020-08-23 12:33:29 +0200

    Move helper script import before alpine import

--- src/index.js
+++ src/index.js
@@ -7,6 +7,6 @@ import './store.js'
     counter: './components/counter.js',
   })
   
-  import('https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js')
   import('https://cdn.jsdelivr.net/gh/kevinbatdorf/alpine-magic-helpers@0.2.x/dist/index.js')
+  import('https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js')
 })()
