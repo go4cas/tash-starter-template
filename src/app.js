a06d125441a51925644b26f572c0fe6fd7918ad4
Cas du Plessis <go4cas@gmail.com>
2020-08-25 11:00:17 +0200

    Reduce settimeout

--- src/app.js
+++ src/app.js
@@ -5,8 +5,8 @@ import './store.js'
   registerComponents({
   })
   
-  setTimeout(async () => {x
+  setTimeout(async () => {
     await import('https://cdn.jsdelivr.net/gh/kevinbatdorf/alpine-magic-helpers@0.3.x/dist/index.js')
     await import('https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js')  
-  }, 1000)  
+  }, 500)  
 })()
