ae1021606d89ab3d4fe8d6eca2929e685a9838b3
Cas du Plessis <go4cas@gmail.com>
2020-08-27 14:15:37 +0200

    Include tailwind config viewer and update Spruce to 1.x

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import Spruce from 'https://cdn.jsdelivr.net/gh/ryangjchandler/spruce@0.x.x/dist/spruce.module.js'
+import Spruce from 'https://cdn.jsdelivr.net/gh/ryangjchandler/spruce@1.x.x/dist/spruce.module.js'
 
 /*
 Spruce.config({
